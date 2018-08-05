143e21db4348167360bb6ccf349d3d2eb88ad091
Cameron Johnson <cameronrossjohnson@gmail.com>
2018-08-04 19:38:22 -0500

    Added some inputs and styling

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, TextInput, View } from 'react-native';
+
 
 export default class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {url: ''};
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+          <View style={[styles.header]}></View>
+          <View style={[styles.content]}>
+            <Text>Coco Health</Text>
+            <TextInput
+                style={{height: 40}}
+                placeholder="Enter your Survey Monkey URL:"
+                onChangeText={(url) => this.setState({url})}
+            />
+            <Text style={{padding: 10, fontSize: 12}}>
+                {this.state.url}
+            </Text>
+          </View>
       </View>
     );
   }
@@ -16,8 +31,21 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+      flexDirection: 'column',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+    header: {
+        height: 40,
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        backgroundColor: '#03A9F4',
+        zIndex: 10
+    },
+    content: {
+        alignItems: 'center',
+        marginTop: 50,
+        marginBottom: 40
+    }
 });
